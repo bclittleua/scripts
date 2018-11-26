edb946e5f10eefc21b10f2555aac3829d6c4382f
bclittleua <32556428+bclittleua@users.noreply.github.com>
2018-11-26 04:25:16 -0700

    Update discobot.py

--- discobot.py
+++ discobot.py
@@ -6,13 +6,11 @@ import subprocess
 # discord client
 client = discord.Client()
 
-
 # create a new event
 @client.event
 async def on_ready():
      print("BOOP BEEP BOOP SQUEEE!")
      print("(Bot Ready)")
-
 
 # listen for specific messages
 @client.event
@@ -22,12 +20,8 @@ async def on_message(message):
 
 
     if message.content.startswith("/uptime"):
-        #args = process_message(message)
-        #await client.send_message(message.channel, "Stand by.")
         result = subprocess.check_output(['/home/pi/bin/man/manbc.sh'])
         #await client.send_message(message.channel, "Stand by.")
-
-
 
 # run the bot
 bot = "YOUR_DISCORD_BOT_TOKEN_GOES_HERE"
