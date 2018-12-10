efb8a60bbf6b9cd70cebec9b32eeda29af067d10
bclittleua <32556428+bclittleua@users.noreply.github.com>
2018-12-10 10:08:01 -0700

    Update discobot.py

--- swmud/discobot.py
+++ swmud/discobot.py
@@ -4,8 +4,8 @@ import os
 import subprocess
 
 # files written after execution of subprocess scripts
-uptime = open('/home/pi/bin/mud/sw-up.log').read()
-reboot = open('/home/pi/bin/mud/sw-rb.log').read()
+#uptime = open('/home/pi/bin/mud/sw-up.log').read()
+#reboot = open('/home/pi/bin/mud/sw-up.log').read()
 
 # discord client
 client = discord.Client()
@@ -22,10 +22,10 @@ async def on_message(message):
         await client.send_message(message.channel, "Need help? Try /uptime for time since reboot, /reboot for time until reboot, and /helpme to see this message.")
     if message.content.startswith("/uptime"):
         result = subprocess.check_output(['/home/pi/bin/mud/mudup.sh']);
-        await client.send_message(message.channel, "The MUD has been up for " + uptime)  
+        await client.send_message(message.channel, "The MUD has been up for " + open('/home/pi/bin/mud/sw-up.log').read())  
     if message.content.startswith("/reboot"):
         result = subprocess.check_output(['/home/pi/bin/mud/mudbc.sh']);
-        await client.send_message(message.channel, "The MUD will restart in " + reboot)  
+        await client.send_message(message.channel, "The MUD will restart in " + open('/home/pi/bin/mud/sw-up.log').read())  
 
 # run the bot
 bot = "BOT-TOKEN-GOES-HERE"
