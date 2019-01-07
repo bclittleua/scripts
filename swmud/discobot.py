c51b63b4f224d90291339b1a89e614b2e5dab347
bclittleua <32556428+bclittleua@users.noreply.github.com>
2019-01-07 07:02:57 -0700

    Update discobot.py

--- swmud/discobot.py
+++ swmud/discobot.py
@@ -22,9 +22,11 @@ async def on_ready():
 # listen for specific messages
 @client.event
 async def on_message(message):
-    if message.content.startswith("/helpme"):
-        await client.send_message(message.channel, "Need help? Try /uptime for time since reboot, /reboot for time until reboot, and /helpme to see this message.")
+    if message.content.startswith("/panic"):
+        await client.send_message(message.channel, "Need help? Try:\n"+"/uptime for time since reboot\n"+"/reboot for time until reboot and\n"+"/panic to see this message.")
     if message.content.startswith("/uptime"):
+        # runs an offline script to calculate time since boot, then writes value to file
+        # after script runs, send a canned message to channel and append the above value
         result = subprocess.check_output(['/home/pi/bin/mud/mudup.sh']);
         await client.send_message(message.channel, "The MUD has been up for " + open('/home/pi/bin/mud/sw-up.log').read())  
     if message.content.startswith("/reboot"):
