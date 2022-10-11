1bb369f5953151f47e48d21888b3263fdead0aeb
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-10-11 14:10:16 -0700

    Update umod.sh

--- umod.sh
+++ umod.sh
@@ -1,4 +1,3 @@
-
 sudo usermod user01 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
 sudo usermod user02 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
 sudo usermod user03 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
