58a964e4ea6beeadb79a275d2fb790d5423eb202
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-10-11 13:51:21 -0700

    Update umod.sh

--- umod.sh
+++ umod.sh
@@ -1,3 +1,7 @@
+sudo useradd -m -p <be201440#> -s /bin/bash user01
+sudo useradd -m -p <be201440#> -s /bin/bash user02
+sudo useradd -m -p <be201440#> -s /bin/bash user03
+sudo useradd -m -p <be201440#> -s /bin/bash user04
 sudo usermod user01 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
 sudo usermod user02 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
 sudo usermod user03 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
