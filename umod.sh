4a350112c4838cce548dc4e99371df3bf86aac3a
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-10-11 13:44:21 -0700

    Create umod.sh

---
+++ umod.sh
@@ -0,0 +1,4 @@
+sudo usermod user01 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
+sudo usermod user02 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
+sudo usermod user03 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
+sudo usermod user04 -a -G user00,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
